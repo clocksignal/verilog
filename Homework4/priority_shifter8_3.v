901bc25936c5e1fddc76fa2039c811b47bcf7d96
plamen235BG <91587372+plamen235BG@users.noreply.github.com>
2024-08-06 01:04:30 +0300

    Update priority_shifter8_3.v

--- Homework4/priority_shifter8_3.v
+++ Homework4/priority_shifter8_3.v
@@ -13,7 +13,7 @@ module priority_shifter8_3(
     and(OUTm[3],!IN[7],!IN[6],!IN[5],!IN[4],IN[3]);
     and(OUTm[2],!IN[7],!IN[6],!IN[5],!IN[4],!IN[3],IN[2]);
     and(OUTm[1],!IN[7],!IN[6],!IN[5],!IN[4],!IN[3],!IN[2],IN[1]);
-    and(OUTm[0],!IN[7],!IN[6],!IN[5],!IN[4],!IN[3],!IN[2],!IN[1],IN[0]);
+#    and(OUTm[0],!IN[7],!IN[6],!IN[5],!IN[4],!IN[3],!IN[2],!IN[1],IN[0]);
     
     or(OUT[2],OUTm[4],OUTm[5],OUTm[6],OUTm[7]);
     or(OUT[1],OUTm[2],OUTm[3],OUTm[6],OUTm[7]);
